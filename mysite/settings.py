9a26cbc895494a7830a43491f7b57e24bf66a5ff
shoddeji <ashodunke1@ivytech.edu>
2024-12-07 06:20:32 -0500

    Added www.dejishod.com to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 LOGIN_REDIRECT_URL = '/'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', 'www.dejishod.com']
 
 
 # Application definition
